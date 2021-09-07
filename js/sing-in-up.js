439fe451e226798b2840c99570c205041b250437
kamrulhaider <kamrulhaider.ssb@gmail.com>
2021-09-07 14:00:34 +0600

    final submission

---
+++ js/sing-in-up.js
@@ -0,0 +1,7 @@
+function addDiv() {
+    document.getElementById('business-name').classList.remove('d-none');
+};
+
+function removeDiv() {
+    document.getElementById('business-name').classList.add('d-none');
+};
